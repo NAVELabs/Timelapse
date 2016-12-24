8d34eecc77e9c0507f3b51955e8aca66912d27a5
Lucas Soares <lsoares267@gmail.com>
2016-12-24 14:51:00 -0200

    Adicionando código arduino

---
+++ timelapse.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+
+#define SERVO 8
+#define POTENCIOMETRO A0 
+ 
+Servo s;
+int pos; 
+
+int valuePot = 0;
+ 
+void setup ()
+{
+  s.attach(SERVO);
+}
+ 
+void loop()
+{
+  for(pos = 0; pos <= 180; pos++)
+  {
+     movement();
+  }
+
+  for(pos = 180; pos >= 0; pos--)
+  {
+     movement();
+  }
+}
+
+void movement()
+{
+    valuePot = map(analogRead(POTENCIOMETRO),0,1023,500,2500); 
+    s.write(pos);
+    delay(valuePot);
+}
